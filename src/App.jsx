9715e731c06354f1ddb8b25db4b4fa2b156f55c7
Gonzalo <34680110ax@gmail.com>
2023-03-10 14:16:18 -0300

    seagrega el proyecto

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import { useForm } from 'react-hook-form'
+
+function App() {
+  const {register, handleSubmit} = useForm()
+
+  const onSubmit =(data)=>{
+    console.log(data)
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input type="text"  />
+      <button></button>
+    </form>
+  )
+}
+
+export default App
